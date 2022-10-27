c25b7564c5369804fbba8ceb997a0b8673693bfa
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-10-27 12:38:42 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Profile from "./components/Profile";
 import Links from "./components/Links";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Profile />
       <Links />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import img1 from '../images/zuri vector.png'
+import img2 from '../images/I4G.png'
+
+const Footer = () => {
+  return (
+    <div id='footer'>
+        <img src={img1} alt='' />
+        <p>HNG Internship 9 Frontend Task</p>
+        <img src={img2} alt='' />
+    </div>
+  )
+}
+
+export default Footer
